135847552e3a851f04dc17b84b2ac35b39d59585
tameralshazly <tameralshazly@yahoo.com>
2023-07-25 18:48:35 +0400

    step-01

---
+++ android/app/src/main/kotlin/com/example/detailsofprovider/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.detailsofprovider
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,28 @@
+import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+import 'package:uuid/uuid.dart';
+
+void main() {
+  runApp(
+    MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: const HomePage(),
+    ),
+  );
+}
+
+class HomePage extends StatelessWidget {
+  const HomePage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Home Page'),
+      ),
+    );
+  }
+}
